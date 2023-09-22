950d9259de5275b3cfaf3e72572c716be960ba57
Bernardo Heduvan <berni.heduvan1@hotmail.com>
2023-09-21 21:48:34 -0300

    rickandmorty-back-end-base-de-datos

--- Servidor/src/routes/index.js
+++ Servidor/src/routes/index.js
@@ -6,19 +6,19 @@ const {postFav} = require('../controllers/postFav');
 const {login} = require('../controllers/login');
 const {deleteFav} = require('../controllers/deleteFav');
 
-router.get("/character/:id", (req, res) => {
+router.get("/character/:id",(req, res) => {
     getCharById(req, res);
 });
-router.get("/login", (req, res) => {
+router.get("/login",(req, res) => {
     login(req, res);
 });
-router.post("/login", (req, res) => {
+router.post("/login",(req, res) => {
     postUser(req, res);
 });
-router.post("/fav", (req, res) => {
+router.post("/fav",(req, res) => {
     postFav(req, res);
 });
-router.delete("/fav/:id", (req, res) => {
+router.delete("/fav/:id",(req, res) => {
     deleteFav(req, res);
 });
 
